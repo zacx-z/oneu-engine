f084024c8bfd5a47643d7e99f6fef92d8c02dacc
ladaceZ@gmail.com <ladaceZ@gmail.com@d30a3831-f586-b1a3-add9-2db6dc386f9c>
2011-04-13 03:13:47 +0000

    A formal one.

--- Utility/Feng/Feng.cpp
+++
@@ -1,9 +0,0 @@
-#include "FengAlgo.h"
-namespace OneU
-{
-	dword Cross( const CLine2 &l1, const CLine2 &l2 )
-	{
-		return ( ( l1.p1 >> l1.p2 ) % ( l1.p1 >> l2.p1 ) ) * ( ( l1.p1 >> l1.p2 ) % ( l1.p1 >> l2.p2 ) ) < 0
-			&& ( ( l2.p1 >> l2.p2 ) % ( l2.p1 >> l1.p1 ) ) * ( ( l2.p1 >> l2.p2 ) % ( l2.p1 >> l1.p2 ) ) < 0;
-	}
-}
--- 废弃/a/BaseLayer.h
+++
@@ -1,1 +0,0 @@
-#include "../BaseLayer/BaseLayer.h"
